934992bc127d3eb5dd8629e8efa933858e064648
Jiayu Yi <yijiayu@gmail.com>
2022-11-05 09:36:55 +0800

    Add models to admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
+from django.apps import apps
 from django.contrib import admin
 
-# Register your models here.
+app = apps.get_app_config("app")
+for model in app.get_models():
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
